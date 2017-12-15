30f3627344700d75f83f20830ee5d0de824cb0b1
Greg Book <gregory.book@hhchealth.org>
2017-12-15 11:00:09 -0500

    fix setup script

--- setup/setup-centos7.sh
+++ setup/setup-centos7.sh
@@ -4,18 +4,30 @@
 
 # if you change the default path, NiDB is not guaranteed to work correctly
 NIDBROOT="/nidb"
+WWWROOT="/var/www/html"
+NIDBUSER="nidb"
+MYSQLUSER="nidb"
+MYSQLPASS="password"
+MYSQLROOTPASS="password"
 
 clear
 echo 
 echo 
 echo 
-echo "******************************************************"
+echo "************************************************************"
 echo 
-echo "           " Neuroimaging Database Setup
+echo "              " Neuroimaging Database Setup
 echo 
-echo "     (installing in ${NIDBROOT} and /var/www/html)"
+echo "   Installing using the following variables:"
+echo "       NIDBROOT: ${NIDBROOT}"
+echo "       WWWROOT: ${WWWROOT}"
+echo "       NIDBUSER: ${NIDBUSER}"
+echo "       MYSQLUSER: ${MYSQLUSER}"
+echo "       MYSQLPASS: ${MYSQLPASS}"
+echo "       MYSQLROOTPASS: ${MYSQLROOTPASS}"
 echo 
-echo "  Maximize this terminal window to read all instructions"
+echo "   If you would like to change these variables, exit this"
+echo "   script, edit the variables and run this script again"
 echo 
 echo "******************************************************"
 echo 
@@ -26,34 +38,38 @@ echo
 read -p "Press [enter] to continue"
 
 #---------------create an nidb account --------------
-echo "${redb}${whitef}${boldon}--------------- Creating nidb user account ----------------${reset}"
+echo "--------------- Creating nidb user account ----------------"
 echo "Please enter the Linux account under which NiDB will run (it will be created if it does not exist)"
 read NIDBUSER
 useradd -m -s /bin/bash $NIDBUSER
 echo "Enter the password for the NiDB account"
 passwd $NIDBUSER
 
+# enable repos for PHP7 and mariadb 10
+yum install -y -q https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y -q http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install -y -q yum-utils
+yum-config-manager --enable remi-php72   # Install PHP 7.2
+
+echo "# MariaDB 10.2 CentOS repository list - created 2017-12-15 15:51 UTC
+# http://downloads.mariadb.org/mariadb/repositories/
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.2/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1" >> /etc/yum.repos.d/MariaDB.repo
+
 # ---------- yum based installs ----------
 echo "----------------- Installing YUM based packages -----------------"
-echo "Because of Perl dependency issues, all perl packages will be installed"
+echo "Installing Perl yum packages"
 yum install -y -q vim
 yum install -y -q perl*
 yum install -y -q cpan
-yum install -y -q php
-yum install -y -q php-mysql
-yum install -y -q php-gd
-yum install -y -q php-process
-yum install -y -q php-pear
-yum install -y -q php-mcrypt
-yum install -y -q php-mbstring
+yum install -y -q php php-mysql php-gd php-process php-pear php-mcrypt php-mbstring
 yum install -y -q httpd
-yum install -y -q mysql
-yum install -y -q mysql-server
-yum install -y -q mariadb
-yum install -y -q mariadb-server
+yum install -y -q MariaDB-server MariaDB-client
 yum install -y -q git
-yum install -y -q gcc
-yum install -y -q gcc-c++
+yum install -y -q gcc gcc-c++
 yum install -y -q gedit*
 yum install -y -q iptraf*
 yum install -y -q java
@@ -87,13 +103,10 @@ pear install Net_SMTP
 cp -rv Mysql* /usr/local/lib64/perl5/
 
 # ---------- configure system based services ----------
-echo "----------------- Configuring system services -----------------"
-echo "------ Disable SELinux ------"
+echo "----------------- Configuring system services -----------------\n"
+echo "------ Disabling SELinux ------"
 setenforce 0
-#echo "SELinux is now temporarily disabled. To permanently disable it, the following must be done"
-#echo "This step must be done manually. edit /etc/selinux/config and change SELINUX=enforcing to SELINUX=disabled"
 sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
-#read -p "Press [enter] to continue"
 
 echo "Setting up port forwarding to forward 8104 to 104"
 # configure the firewall to accept everything, and still forward port 104 to 8104 (using iptables)
@@ -119,9 +132,6 @@ echo "Done setting up port forwarding and disabling the firewall"
 
 # ---------- Web based installs ----------
 echo "----------------- Web based installs (Webmin, phpMyAdmin) -----------------"
-#echo "------ Installing Webmin... ------"
-#wget http://prdownloads.sourceforge.net/webadmin/webmin-1.750-1.noarch.rpm
-#rpm -U webmin-1.750-1.noarch.rpm
 
 echo "------ Enabling services at boot ------"
 systemctl enable httpd.service
@@ -129,10 +139,6 @@ systemctl enable mariadb.service
 echo "------ Starting services ------"
 systemctl start httpd.service
 systemctl start mariadb.service
-
-#echo "------ Manually configure PHP variables ------"
-#echo "Go to https://$HOSTNAME:10000"
-#echo "then go to Others -> PHP Configuration -> Manage -> Other Settings ... change PHP Timezone to your timezone"
 
 sed -i 's/^short_open_tag = .*/short_open_tag = On/g' /etc/php.ini
 sed -i 's/^session.gc_maxlifetime = .*/session.gc_maxlifetime = 28800/g' /etc/php.ini
@@ -144,7 +150,7 @@ sed -i 's/^max_input_time = .*/max_input_time = 600/g' /etc/php.ini
 sed -i 's/^max_execution_time = .*/max_execution_time = 600/g' /etc/php.ini
 sed -i 's/^post_max_size = .*/post_max_size = 5000M/g' /etc/php.ini
 sed -i 's/^display_errors = .*/display_errors = On/g' /etc/php.ini
-sed -i 's/^error_reporting = .*/error_reporting = E_ALL & \~E_DEPRECATED & \~E_STRICT & \~E_NOTICE/' /etc/php.ini
+sed -i 's/^error_reporting = .*/error_reporting = E_ALL \& \~E_DEPRECATED \& \~E_STRICT \& \~E_NOTICE/' /etc/php.ini
 
 echo "------ Modifying httpd to run as nidb user ------"
 sed -i "s/User apache/User $NIDBUSER/" /etc/httpd/conf/httpd.conf
@@ -154,16 +160,19 @@ echo "------ Restarting httpd ------"
 systemctl restart httpd.service
 
 echo "------ Setting up MySQL database - default password is 'password' ------"
-mysqladmin -u root password 'password'
+mysqladmin -u root password '${MYSQLROOTPASS}'
 echo "Assigning permissions to mysql root account"
 echo "GRANT ALL PRIVILEGES on *.* to root@'%'" >> ~/tempsql.txt
-mysql -uroot -ppassword < ~/tempsql.txt
+echo "CREATE USER '${MYSQLUSER}'@'%' identified by '${MYSQLPASS}'" >> ~/tempsql.txt
+echo "GRANT ALL PRIVILEGES on *.* to root@'%'" >> ~/tempsql.txt
+mysql -uroot -p${MYSQLROOTPASS} < ~/tempsql.txt
 rm ~/tempsql.txt
 
 echo "------ Install phpMyAdmin ------"
-wget http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.4.7/phpMyAdmin-4.4.7-english.zip
-unzip phpMyAdmin-4.4.7-english.zip
-mv phpMyAdmin-4.4.7-english /var/www/html/phpMyAdmin
+#wget http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.4.7/phpMyAdmin-4.4.7-english.zip
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.zip
+unzip phpMyAdmin-latest-english.zip
+mv phpMyAdmin-latest-english /var/www/html/phpMyAdmin
 chmod 777 /var/www/html
 chown -R $NIDBUSER:$NIDBUSER /var/www/html
 sed '$ i $cfg[''McryptDisableWarning''] = TRUE;' /var/www/html/phpMyAdmin/config.sample.inc.php;
@@ -213,16 +222,6 @@ mysql -uroot -ppassword nidb < nidb.sql
 # ---------- dcm4che ----------
 echo "----------------- Installing DICOM receiver -----------------"
 echo "Installing dcm4che receiver to listen on port 8104"
-#echo "Go to Webmin -> System -> Bootup and Shutdown"
-#echo "     Click 'create a new bootup and shutdown action'"
-#echo "     Enter the following and click 'Create'"
-#echo "     Name: dcmrcv"
-#echo "     Description: dcmrcv"
-#echo "     Bootup commands: su nidb -c '${NIDBROOT}/programs/dcm4che/bin/./dcmrcv NIDB:8104 -dest ${NIDBROOT}/dicomincoming > /dev/null 2>&1 &'"
-#echo "     Shutdown commands: "
-#echo "     Start at boot time: Yes"
-#read -p "Press [enter] to continue"
-
 # add dcmrcv service at boot
 cp ${NIDBROOT}/install/programs/dcmrcv /etc/init.d
 sed -i "s/su nidb/su $NIDBUSER/" /etc/init.d/dcmrcv
@@ -244,9 +243,9 @@ rm ~/tempcron.txt
 
 # ---------- list the remaining things to be done by the user ----------
 echo "----------------- Remaining items to be done by you -----------------"
-echo " *** Install FSL to the default path [/usr/local/fsl] ***"
-echo "Edit /etc/php.ini to reflect your timezone"
-echo "Your default mysql account is root, password is 'password'. Change these as soon as possible"
-echo "Edit ${NIDBROOT}/programs/nidb.cfg.sample to reflect your paths, usernames, and passwords. Rename to nidb.cfg"
-echo "Some modules are disabled in cron. Use crontab -e to enable them"
+echo "1) Install FSL to the default path [/usr/local/fsl] ***"
+echo "2) Edit /etc/php.ini to reflect your timezone"
+echo "3) Your default mysql account is root, password is '${MYSQLROOTPASS}'. Change these as soon as possible"
+echo "4) Edit ${NIDBROOT}/programs/nidb.cfg.sample to reflect your paths, usernames, and passwords. Rename to nidb.cfg"
+echo "5) Some modules are disabled by default in cron. Use crontab -e to enable them"
 echo "TIP: A reboot can be useful to make sure everything works"
