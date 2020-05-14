bb1c8b68729e02100269d12e895939121dd4191f
Greg Book <gregory.book@hhchealth.org>
2020-05-14 13:00:57 -0400

    build fixes

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ SRCDIR=/nidb/nidbgit/src
 
 
 # this script requires make, cmake3, and qmake
-command -v make >/dev/null 2>&1 || { echo >&2 "This script requires make, but it's not installed"; exit 1; }
-command -v gcc >/dev/null 2>&1 || { echo >&2 "This script requires gcc, but it's not installed"; exit 1; }
+command -v make >/dev/null 2>&1 || { echo >&2 "\nThis script requires make, but it's not installed\n"; exit 1; }
+command -v gcc >/dev/null 2>&1 || { echo >&2 "\nThis script requires gcc, but it's not installed\n"; exit 1; }
 
 # create the build directory
 echo "Creating build directory"
@@ -18,32 +18,32 @@ mkdir -p $BUILDDIR
 
 # build gdcm (make sure cmake3 is installed)
 if [ ! -d "$BUILDDIR/gdcm" ]; then
-	command -v cmake3 >/dev/null 2>&1 || { echo >&2 "This script requires cmake3, but it's not installed. Install using 'yum install cmake3'."; exit 1; }
+	command -v cmake3 >/dev/null 2>&1 || { echo >&2 "\nThis script requires cmake3, but it's not installed. Install using 'yum install cmake3'.\n"; exit 1; }
 
-	echo "gdcm not built. Building gdcm now"
+	echo "\ngdcm not built. Building gdcm now\n"
 
 	mkdir -p $BUILDDIR/gdcm
 	cd $BUILDDIR/gdcm
-	cmake3 $SRCDIR/gdcm
+	cmake3 -DGDCM_BUILD_SHARED_LIBS:STRING=YES -DGDCM_BUILD_TESTING:STRING=NO -DGDCM_BUILD_EXAMPLES:STRING=NO $SRCDIR/gdcm
 	make
 else
-	echo "gdcm already built. Using $BUILDDIR/gdcm"
+	echo "\ngdcm already built. Using $BUILDDIR/gdcm\n"
 fi
 
 # build smtp module
 if [ ! -d "$BUILDDIR/smtp" ]; then
 
-	echo "smtp module not built. Building smtp module now"
+	echo "\nsmtp module not built. Building smtp module now\n"
 
 	$QMAKEBIN -o $BUILDDIR/smtp/Makefile $SRCDIR/smtp/SMTPEmail.pro -spec linux-g++
 	cd $BUILDDIR/smtp
 	make
 else
-	echo "smtp already built. Using $BUILDDIR/gdcm"
+	echo "\nsmtp already built. Using $BUILDDIR/smtp\n"
 fi
 
 # build NiDB core
-echo "Building NiDB core"
+echo "\nBuilding NiDB core\n"
 # create make file in the build directory
 $QMAKEBIN -o $BUILDDIR/nidb/Makefile $SRCDIR/nidb/nidb.pro -spec linux-g++ # CONFIG+=qtquickcompiler
 cd $BUILDDIR/nidb
