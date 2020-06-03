5f788bffc7c3438341128df642f97a9cfc45fb28
Greg Book <gregory.book@hhchealth.org>
2020-06-03 12:29:00 -0400

    build change before rpm build

--- src/nidb/build.sh
+++ src/nidb/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash    
-n=87;#the variable that I want to be incremented
+n=496;#the variable that I want to be incremented
 next_n=$[$n+1]
 sed -i "/#the variable that I want to be incremented$/s/=.*#/=$next_n;#/" ${0}
 echo $n
