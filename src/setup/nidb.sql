b8f325a207f8479393547b9c8f72e2d900f658a4
Greg Book <gregory.a.book@gmail.com>
2022-03-14 14:45:40 -0400

    update schema

--- src/setup/nidb.sql
+++ src/setup/nidb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 17, 2022 at 04:12 PM
+-- Generation Time: Mar 14, 2022 at 06:40 PM
 -- Server version: 10.3.28-MariaDB
 -- PHP Version: 7.2.24
 
