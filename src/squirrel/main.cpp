d63cb580046efb9f62db20953ffebc37b749eebc
Gregory Book <gregory.a.book@gmail.com>
2022-05-17 15:37:54 -0400

    squirrel command line options

--- src/squirrel/main.cpp
+++ src/squirrel/main.cpp
@@ -43,61 +43,42 @@ int main(int argc, char *argv[])
     p.addPositionalArgument("tool", "Available tools:  dicom2squirrel  validate");
 
     /* command line flag options */
-    QCommandLineOption optDebug(QStringList() << "d" << "debug", "Enable debugging");
+	QCommandLineOption optDebug(QStringList() << "debug", "Enable debugging");
     QCommandLineOption optQuiet(QStringList() << "q" << "quiet", "Dont print headers and checks");
     p.addOption(optDebug);
     p.addOption(optQuiet);
 
     /* command line options that take values */
-    QCommandLineOption optAnalysisID(QStringList() << "a" << "analysisid", "resultinsert -or- pipelinecheckin submodules only", "analysisid");
-    QCommandLineOption optStatus(QStringList() << "s" << "status", "pipelinecheckin submodule", "status");
-    QCommandLineOption optMessage(QStringList() << "m" << "message", "pipelinecheckin submodule", "message");
-    QCommandLineOption optCommand(QStringList() << "c" << "command", "pipelinecheckin submodule", "command");
-    QCommandLineOption optResultText(QStringList() << "t" << "text", "Insert text result (resultinsert submodule)", "text");
-    QCommandLineOption optResultNumber(QStringList() << "n" << "number", "Insert numerical result (resultinsert submodule)", "number");
-    QCommandLineOption optResultFile(QStringList() << "f" << "file", "Insert file result (resultinsert submodule)", "filepath");
-    QCommandLineOption optResultImage(QStringList() << "i" << "image", "Insert image result (resultinsert submodule)", "imagepath");
-    QCommandLineOption optResultDesc(QStringList() << "e" <<"desc", "Result description (resultinsert submodule)", "desc");
-    QCommandLineOption optResultUnit(QStringList() << "unit", "Result unit (resultinsert submodule)", "unit");
-    p.addOption(optAnalysisID);
-    p.addOption(optStatus);
-    p.addOption(optMessage);
-    p.addOption(optCommand);
-    p.addOption(optResultText);
-    p.addOption(optResultNumber);
-    p.addOption(optResultFile);
-    p.addOption(optResultImage);
-    p.addOption(optResultDesc);
-    p.addOption(optResultUnit);
+	QCommandLineOption optDicomDir(QStringList() << "d" << "dicomdir", "Path to directory containing DICOM files", "dicomdir");
+	QCommandLineOption optOutputFile(QStringList() << "o" << "out", "Output squirrel file", "out");
+	QCommandLineOption optInputFile(QStringList() << "i" << "in", "Input squirrel file", "in");
+	p.addOption(optDicomDir);
+	p.addOption(optOutputFile);
+	p.addOption(optInputFile);
 
     /* Process the actual command line arguments given by the user */
     p.process(a);
 
-    QString module;
+	QString tool;
     bool debug, quiet;
 
     const QStringList args = p.positionalArguments();
     if (args.size() > 0)
-        module = args.at(0).trimmed();
+		tool = args.at(0).trimmed();
 
     debug = p.isSet(optDebug);
     quiet = p.isSet(optQuiet);
-    QString paramAnalysisID = p.value(optAnalysisID).trimmed();
-    QString paramStatus = p.value(optStatus).trimmed();
-    QString paramMessage = p.value(optMessage).trimmed();
-    QString paramCommand = p.value(optCommand).trimmed();
-    QString paramResultText = p.value(optResultText).trimmed();
-    QString paramResultNumber = p.value(optResultNumber).trimmed();
-    QString paramResultFile = p.value(optResultFile).trimmed();
-    QString paramResultImage = p.value(optResultImage).trimmed();
-    QString paramResultDesc = p.value(optResultDesc).trimmed();
-    QString paramResultUnit = p.value(optResultUnit).trimmed();
+	QString paramDicomDir = p.value(optDicomDir).trimmed();
+	QString paramOutputFile = p.value(optOutputFile).trimmed();
+	QString paramInputFile = p.value(optInputFile).trimmed();
 
     QStringList tools = { "dicom2squirrel", "validate" };
 
     /* now check the command line parameters passed in, to see if they are calling a valid module */
-    if (!tools.contains(module)) {
-        std::cout << QString("Error: unrecognized module [%1]").arg(module).toStdString().c_str();
+	if (!tools.contains(tool)) {
+		if (tool != "")
+			std::cout << QString("Error: unrecognized option [%1]\n").arg(tool).toStdString().c_str();
+
         std::cout << p.helpText().toStdString().c_str();
         return 0;
     }
