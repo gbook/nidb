94639acc3aacc0fc8dd1bfe8e4c95fa86c856e3b
Gregory Book <gregory.a.book@gmail.com>
2022-05-17 10:50:09 -0400

    added version.h

---
+++ src/nidb/version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJ "2022"
+#define VERSION_MIN "5"
+#define BUILD_NUM "805"
+
+#define SQUIRREL_VERSION_MAJ "1"
+#define SQUIRREL_VERSION_MIN "0"
+
+#endif // VERSION_H
