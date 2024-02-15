deeb703be7d7acc4780b6afe60a6394420c22a0e
Gregory Book <gregory.a.book@gmail.com>
2024-02-15 14:19:45 -0500

    new release

--- src/nidb/version.h
+++ src/nidb/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_MAJ "2023"
-#define VERSION_MIN "12"
-#define BUILD_NUM "1041"
+#define VERSION_MAJ "2024"
+#define VERSION_MIN "2"
+#define BUILD_NUM "1061"
 
 #endif // VERSION_H
