1e77658909a07405588618cf26142c48592e76fb
Gregory Book <gregory.a.book@gmail.com>
2024-05-06 16:01:47 -0400

    bit7z build works

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ else
 	BUILDDIR=$3
 fi
 
+BIT7ZDIR=$PWD/bit7z
 ORIGDIR=$PWD
 
 # this script requires make, cmake (3), and qmake
@@ -52,7 +53,7 @@ fi
 
 # ----- build bit7z library -----
 echo -e "\n ----- Building bit7z -----\n"
-mkdir -p $BIT7ZDIR/bbuild
+mkdir -p $BIT7ZDIR/build
 cd $BIT7ZDIR/build
 cmake .. -DBIT7Z_AUTO_FORMAT:BOOL=ON -DBIT7Z_USE_LEGACY_IUNKNOWN=ON -DBIT7Z_GENERATE_PIC=ON -DCMAKE_CXX_FLAGS:STRING=-fPIC -DCMAKE_C_FLAGS:STRING=-fPIC
 cmake --build . --config Release
