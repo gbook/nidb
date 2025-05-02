0a85da356e24149b4c21bed093bb108781d2d592
Gregory Book <gregory.a.book@gmail.com>
2025-05-02 12:48:00 -0400

    release 2025.5.1240

--- src/nidb/version.h
+++ src/nidb/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 #define VERSION_MAJ "2025"
-#define VERSION_MIN "4"
-#define BUILD_NUM "1230"
+#define VERSION_MIN "5"
+#define BUILD_NUM "1240"
 
 #endif // VERSION_H
--- src/setup/nidb.el8.spec
+++ src/setup/nidb.el8.spec
@@ -1,5 +1,5 @@
 Name:           nidb
-Version:        2025.4.1230
+Version:        2025.5.1240
 Release:        1%{?dist}
 Summary:        NeuroInformatics Database
 
