c572130b41a8fdb750dd89e18628bf7242c08701
Gregory Book <gregory.a.book@gmail.com>
2025-09-09 11:49:27 -0400

    release 2025.9.1290

--- build-deb.sh
+++ build-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGE=nidb_2025.8.1280
+PACKAGE=nidb_2025.9.1290
 LIBDIR=$PACKAGE/lib/x86_64-linux-gnu/
 BINDIR=$PACKAGE/usr/local/bin/
 DEBDIR=$PACKAGE/DEBIAN
@@ -19,7 +19,7 @@ cp -uv ~/Qt/6.9.1/gcc_64/lib/libicu* $LIBDIR
 cp -uv bin/nidb/nidb $BINDIR
 
 echo "Package: nidb
-Version: 2025.8.1280
+Version: 2025.9.1290
 Section: base
 Priority: optional
 Architecture: amd64
--- src/nidb/version.h
+++ src/nidb/version.h
@@ -3,6 +3,6 @@
 
 #define VERSION_MAJ "2025"
 #define VERSION_MIN "9"
-#define BUILD_NUM "1280"
+#define BUILD_NUM "1290"
 
 #endif // VERSION_H
