2be1ab2a43ad0cc66b8a02d230cd75fe1a9df081
Gregory Book <gregory.a.book@gmail.com>
2026-09-18 11:10:15 -0400

    modified RHEL installers to use local repo instead of remote

--- makeInstallerRHEL10.sh
+++ makeInstallerRHEL10.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+# build the NiDB .rpm from the local source tree (the git repository containing this script).
+# tracked files are exported, including uncommitted changes; untracked files are not included.
+set -e
+
+SRCDIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SRCDIR"
+SNAPSHOT=$(git stash create) # commit object of the working tree, or empty if there are no local changes
+if [ -n "$SNAPSHOT" ]; then
+	echo "Building from $SRCDIR (HEAD $(git rev-parse --short HEAD) plus uncommitted changes)"
+else
+	SNAPSHOT=HEAD
+	echo "Building from $SRCDIR (HEAD $(git rev-parse --short HEAD))"
+fi
+
 cd ~
-rm -rfv master.zip nidb-master rpmbuild
+rm -rfv rpmbuild
 rpmdev-setuptree
-wget https://github.com/gbook/nidb/archive/master.zip
-unzip master.zip
-mv nidb-master/* rpmbuild/SOURCES/
+git -C "$SRCDIR" archive "$SNAPSHOT" | tar -x -C rpmbuild/SOURCES/
 cp -v rpmbuild/SOURCES/src/setup/nidb.el10.spec rpmbuild/SPECS/
 cd rpmbuild/SPECS
 QA_RPATHS=$((0x0002|0x0010)) rpmbuild -bb nidb.el10.spec
